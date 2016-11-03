35a8f2561c57b142c73f51ae2f8152fa96a58fdd
lucyvinh <lucyvinh@gmail.com>
2016-11-03 15:55:57 -0700

    Updated JS

--- projects/MOOVie/js/scripts.js
+++ projects/MOOVie/js/scripts.js
@@ -1,60 +1,53 @@
-let $happyContainer = $('#emoji-happy');
+let $happyContainer = $('#first-genre-modal');
 
-$('#emoji-happy').on('click', () => {
-  alert('hi');
-  displayResults();
-});
+$('#emoji-happy').on('click', displayResults);
 
 let url = 'https://api.themoviedb.org/3/genre/35/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
 
-$.getJSON(url, function(data) {
+function displayResults(movie) {
+  $.getJSON(url, function(data) {
 
-  movie = data.results;
+    let movies = data.results;
+    console.log(movies);
 
-  displayResults()
-})
+    $happyContainer.empty();
 
-function displayResults() {
+    for (i = 0; i < movies.length; i++) {
 
-  $happyContainer.empty();
+      let movie = movies[i];
 
-  for (i = 0; i < movie.length; i++) {
+      let movieTitle = movie.title;
+      let movieImage = movie.poster_path;
+      let movieSummary = movie.overview;
+      let movieRelease = movie.release_date;
+      // TODO: get API from another source for the following:
+      // let movieRating = movie.;
+      // let movieSimilar = movie.;
+      // http://www.omdbapi.com/ will give you rotten tomato rating and imdb rating if it's available
 
-    let movie = movie.movie;
+      $happyContainer.append(`
+        <h2>Movie Title: ${movieTitle}</h2>
+        <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
+        <p>Summary: ${movieSummary}</p>
+        <p>Release Date: ${movieRelease}</p>
+      `);
+    }
 
-    let movieTitle = movie.title;
-    let movieImage = movie.poster_path;
-    let movieSummary = movie.overview;
-    let movieRelease = movie.release_date;
-    //  TODO: get API from another source for the following:
-    //  let movieRating = movie.;
-    // let movieSimilar = movie.;
+  });
+}
+//$('#firstGenreModal').append(movieTitle + movieImage + movieSummary + movieRelease);
 
-    $happyContainer.append(`
-      <h2>Movie Title: ${movieTitle}</h2>
-      <img src="${movieImage}" alt="${movieTitle}" />
-      <p>Summary: <${movieSummary}</p>
-      <p>Release Date: ${movieRelease}</p>
-      `)
-    )};
-  }
-
-
-
-
-  //$('#firstGenreModal').append(movieTitle + movieImage + movieSummary + movieRelease);
-
-  // $(document).ready(function() {});
-  //
-  // function emoji(feeling) {
-  //        if (feeling == 1){
-  //        $('#genre-question .col-md-12').html("You're happy. What do you feel like watching?");
-  //        if (feeling == 2){
-  //        $('#genre-question .col-md-12').html("You're sad. What do you feel like watching?");}
-  //        if (feeling == 3){
-  //        $('#genre-question .col-md-12').html("You're angry. What do you feel like watching?");}
-  //        if (feeling == 4){
-  //        $('#genre-question .col-md-12').html("You're infatuated. What do you feel like watching?");
-  //        }
-  // }
-  // });
+// $(document).ready(function() {});
+//
+// function emoji(feeling) {
+//        if (feeling == 1){
+//        $('#genre-question .col-md-12').html("You're happy. What do you feel like watching?");
+//        if (feeling == 2){
+//        $('#genre-question .col-md-12').html("You're sad. What do you feel like watching?");}
+//        if (feeling == 3){
+//        $('#genre-question .col-md-12').html("You're angry. What do you feel like watching?");}
+//        if (feeling == 4){
+//        $('#genre-question .col-md-12').html("You're infatuated. What do you feel like watching?");
+//        }
+// }
+// });
