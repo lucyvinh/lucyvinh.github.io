b8a6b603feeb545406bbb21d2becac961bba4283
lucyvinh <lucyvinh@gmail.com>
2016-11-04 12:19:55 -0700

    Updated CSS, added in second movie results

--- projects/MOOVie/js/scripts.js
+++ projects/MOOVie/js/scripts.js
@@ -1,40 +1,5 @@
 let $happyContainer = $('#first-genre-modal');
-
-// let $paginationContainer = $('#pagination-container');
-// let $prevBtn = $('#prev-btn');
-// let $nextBtn = $('#next-btn');
-//
-// const MAX_RESULTS = 10;
-// let matches;
-// let currentIndex;
-//
-// $nextBtn.click(() => {
-//
-//   // check if user goes too far to the right
-//   if (matches.length % MAX_RESULTS !== 0
-//   && currentIndex + MAX_RESULTS > matches.length - MAX_RESULTS) {
-//       currentIndex += matches.length % MAX_RESULTS;
-//
-//   } else {
-//     currentIndex += MAX_RESULTS;
-//   }
-//
-//   displayResults();
-//
-// });
-//
-// $prevBtn.click(() => {
-//
-//   if (currentIndex === matches.length - MAX_RESULTS) {
-//     currentIndex -= matches.length % MAX_RESULTS;
-//   } else {
-//     currentIndex -= MAX_RESULTS;
-//   }
-//
-//   displayResults();
-//
-// });
-
+let $secondHappyContainer = $(`#second-genre-modal`);
 
 $('#emoji-happy').on('click', displayResults);
 
@@ -58,49 +23,68 @@ function displayResults(movie) {
       let movieRelease = movie.release_date;
       // TODO: get API from another source for the following:
       // movie similar API request
-      let similarMovieUrl = 'https://api.themoviedb.org/3/movie/movieID/similar?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US'
-      // let movieRating = movie.;
+      let similarMovieUrl = 'https://api.themoviedb.org/3/movie/movieID/similar?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US';
+      let movieRating = 'https://api.themoviedb.org/3/review/movie.id?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b';
+
       // http://www.omdbapi.com/ will give you rotten tomato rating and imdb rating if it's available
 
       $happyContainer.append(`
-        <h2>Movie Title: ${movieTitle}</h2>
+        <h3>Movie Title: ${movieTitle}</h3>
         <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
-        <p>Summary: ${movieSummary}</p>
-        <p>Release Date: ${movieRelease}</p>
-        <p>Similar Movies:
+        <p><strong>Summary:</strong> ${movieSummary}</p>
+        <p><strong>Release Date:</strong> ${movieRelease}</p>
+        <p><strong>Rotten Tomato Rating:</strong> ${movieRating}</p>
+        <div class="btn btn-sm btn-success" data-toggle="modal" data-target="${similarMovieUrl}">Similar Movies</div><br>
         `);
       }
 
     });
-      // displayPaginationButtons();
+
   }
 
-  // function displayPaginationButtons() {
-  //   console.log('displayPaginationButtons');
-  //   console.log('currentIndex: ', currentIndex);
-  //   console.log('matches.length: ', matches.length);
-  //   console.log('MAX_RESULTS: ', MAX_RESULTS);
-  //   console.log('matches.length - MAX_RESULTS: ' + (matches.length - MAX_RESULTS));
-  //
-  //   // $nextBtn
-  //   if (matches.length > MAX_RESULTS) {
-  //     $nextBtn.removeClass('invisible');
-  //   }
-  //   // makes nextBtn invisible if user goes too far to the right
-  //   if (currentIndex >= (matches.length - MAX_RESULTS)) {
-  //     $nextBtn.addClass('invisible');
-  //   }
-  //
-  //   // $prevBtn
-  //   if (currentIndex <= MAX_RESULTS) {
-  //     $prevBtn.removeClass('invisible');
-  //   }
-  //   if (currentIndex === 0) {
-  //     $prevBtn.addClass('invisible');
-  //   }
-  //
-  //
-  // }
+
+//second Happy genre function
+$('#emoji-happy').on('click', displaySecondResults);
+
+let secondUrl = 'https://api.themoviedb.org/3/genre/878/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
+
+function displaySecondResults(movie) {
+  $.getJSON(secondUrl, function(data) {
+
+    let movies = data.results;
+    console.log(movies);
+
+    $secondHappyContainer.empty();
+
+    for (i = 0; i < movies.length; i++) {
+
+      let movie = movies[i];
+      let movieID = movie.id;
+      let movieTitle = movie.title;
+      let movieImage = movie.poster_path;
+      let movieSummary = movie.overview ? movie.overview : '<p>Summary not available.</p>';
+      let movieRelease = movie.release_date;
+      // TODO: get API from another source for the following:
+      // movie similar API request
+      let similarMovieUrl = 'https://api.themoviedb.org/3/movie/movieID/similar?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US';
+      let movieRating = 'https://api.themoviedb.org/3/review/movie.id?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b';
+
+      // http://www.omdbapi.com/ will give you rotten tomato rating and imdb rating if it's available
+
+      $secondHappyContainer.append(`
+        <h3>Movie Title: ${movieTitle}</h3>
+        <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}"/><br>
+        <p><strong>Summary:</strong> ${movieSummary}</p>
+        <p><strong>Release Date:</strong> ${movieRelease}</p>
+        <p><strong>Rotten Tomato Rating:</strong> ${movieRating}</p>
+        <div class="btn btn-sm btn-success" data-toggle="modal" data-target="${similarMovieUrl}">Similar Movies</div><br>
+        `);
+      }
+
+    });
+
+  }
+
 
   $(document).ready(function() {});
 
