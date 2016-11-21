ae2def7bfa9bc4a24c428c0d3fee84c1ea2dabf7
lucyvinh <lucyvinh@gmail.com>
2016-11-20 18:18:26 -0800

    Violated DRY principle

--- projects/MOOVie/js/scripts.js
+++ projects/MOOVie/js/scripts.js
@@ -1,5 +1,22 @@
 let $happyContainer = $('#first-genre-modal');
-let $secondHappyContainer = $(`#second-genre-modal`);
+let $sadContainer = $(`#second-genre-modal`);
+let $angryContainer = $(`#third-genre-modal`);
+let $romanceContainer = $(`#fourth-genre-modal`);
+
+$(document).ready(function() {});
+
+function emoji(feeling) {
+  if (feeling == 1){
+    $('#genre-question .col-md-12').html("You're happy. What do you feel like watching?");
+    if (feeling == 2){
+      $('#genre-question .col-md-12').html("You're sad. What do you feel like watching?");}
+      if (feeling == 3){
+        $('#genre-question .col-md-12').html("You're angry. What do you feel like watching?");}
+        if (feeling == 4){
+          $('#genre-question .col-md-12').html("You're infatuated. What do you feel like watching?");
+        }
+      }
+    }
 
 $('#emoji-happy').on('click', displayResults);
 
@@ -12,6 +29,9 @@ function displayResults(movie) {
     console.log(movies);
 
     $happyContainer.empty();
+    $sadContainer.empty();
+    $angryContainer.empty();
+    $romanceContainer.empty();
 
     for (i = 0; i < movies.length; i++) {
 
@@ -23,118 +43,118 @@ function displayResults(movie) {
       let movieRelease = movie.release_date;
 
       $happyContainer.append(`
+
         <h3>Movie Title: ${movieTitle}</h3>
         <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
         <p><strong>Summary:</strong> ${movieSummary}</p>
         <p><strong>Release Date:</strong> ${movieRelease}</p>
-
         `);
+
       }
     })
   }
 
-  function displayRating(rating) {
+  $('#emoji-sad').on('click', displaySecondResults);
+  let secondUrl = 'https://api.themoviedb.org/3/genre/18/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
 
-    let movieRatingUrl = 'http://www.omdbapi.com/?&tomatoes=true&t=' + movieID;
-    $.getJSON(movieRatingUrl, function(data) {
+  function displaySecondResults(movie) {
+    $.getJSON(secondUrl, function(data) {
 
-      let rating = data.results;
+      let movies = data.results;
+      console.log(movies);
 
       $happyContainer.empty();
+      $sadContainer.empty();
+      $angryContainer.empty();
+      $romanceContainer.empty();
 
-      for (i = 0; i < rating.length; i++) {
+      for (i = 0; i < movies.length; i++) {
 
-        let rating = ratings[i];
-        let ratingMeter = ratings.tomatoMeter;
-        let ratingRating = ratings.tomatoRating;
+        let movie = movies[i];
+        let movieID = movie.id;
+        let movieTitle = movie.title;
+        let movieImage = movie.poster_path;
+        let movieSummary = movie.overview ? movie.overview : '<p>Summary not available.</p>';
+        let movieRelease = movie.release_date;
 
-        $happyContainer.append(`
-          <p><strong>Rotten Tomato Meter:</strong> ${ratingMeter}</p>
-          <p><strong>Rotten Tomato Rating:</strong> ${ratingRating}</p>
+        $sadContainer.append(`
+
+          <h3>Movie Title: ${movieTitle}</h3>
+          <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
+          <p><strong>Summary:</strong> ${movieSummary}</p>
+          <p><strong>Release Date:</strong> ${movieRelease}</p>
           `);
-        };
+
+        }
       })
     }
-    
-    function displaySimilarMovie(movieID) {
-      //alert(movieID);
 
-      let similarMovieUrl = 'https://api.themoviedb.org/3/movie/' + movieID +'/similar?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US';
+    $('#emoji-angry').on('click', displayThirdResults);
+    let thirdUrl = 'https://api.themoviedb.org/3/genre/28/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
 
-      alert(similarMovieUrl);
-
-      $.getJSON(similarMovieUrl, function(data) {
+    function displayThirdResults(movie) {
+      $.getJSON(thirdUrl, function(data) {
 
         let movies = data.results;
         console.log(movies);
 
-        //do the loop and container thing again
-        for (i = 0; i < results.length; i++) {
+        $happyContainer.empty();
+        $sadContainer.empty();
+        $angryContainer.empty();
+        $romanceContainer.empty();
+
+        for (i = 0; i < movies.length; i++) {
 
           let movie = movies[i];
-          let similarMovieTitle = results.title
-          let similarMovieImage = results.poster_path
+          let movieID = movie.id;
+          let movieTitle = movie.title;
+          let movieImage = movie.poster_path;
+          let movieSummary = movie.overview ? movie.overview : '<p>Summary not available.</p>';
+          let movieRelease = movie.release_date;
 
-          $happyContainer.append(`
-            <p><strong>Similar Movie Title:</strong> ${similarMovieTitle}</p>
-            <p><img src="http://image.tmdb.org/t/p/w185/${similarMovieImage}" alt="${similarMovieTitle}" /></p>
+          $angryContainer.append(`
 
+            <h3>Movie Title: ${movieTitle}</h3>
+            <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
+            <p><strong>Summary:</strong> ${movieSummary}</p>
+            <p><strong>Release Date:</strong> ${movieRelease}</p>
             `);
 
           }
         })
+      }
 
+      $('#emoji-romantic').on('click', displayFourthResults);
+      let fourthUrl = 'https://api.themoviedb.org/3/genre/10749/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
 
-        // second Happy genre function
-        $('#emoji-happy').on('click', displaySecondResults);
+      function displayFourthResults(movie) {
+        $.getJSON(fourthUrl, function(data) {
 
-        let secondUrl = 'https://api.themoviedb.org/3/genre/878/movies?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US&sort_by=created_at.asc';
+          let movies = data.results;
+          console.log(movies);
 
-        function displaySecondResults(movie) {
-          $.getJSON(secondUrl, function(data) {
+          $happyContainer.empty();
+          $sadContainer.empty();
+          $angryContainer.empty();
+          $romanceContainer.empty();
 
-            let movies = data.results;
-            console.log(movies);
+          for (i = 0; i < movies.length; i++) {
 
-            $secondHappyContainer.empty();
+            let movie = movies[i];
+            let movieID = movie.id;
+            let movieTitle = movie.title;
+            let movieImage = movie.poster_path;
+            let movieSummary = movie.overview ? movie.overview : '<p>Summary not available.</p>';
+            let movieRelease = movie.release_date;
 
-            for (i = 0; i < movies.length; i++) {
+            $romanceContainer.append(`
 
-              let movie = movies[i];
-              let movieID = movie.id;
-              let movieTitle = movie.title;
-              let movieImage = movie.poster_path;
-              let movieSummary = movie.overview ? movie.overview : '<p>Summary not available.</p>';
-              let movieRelease = movie.release_date;
-              // TODO: get API from another source for the following:
-              // movie similar API request
-              let similarMovieUrl = 'https://api.themoviedb.org/3/movie/movieID/similar?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b&language=en-US';
-              let movieRating = 'https://api.themoviedb.org/3/review/movie.id?api_key=9bd1f25c3d0ac4ea5f8d7e4ff01ee51b';
+              <h3>Movie Title: ${movieTitle}</h3>
+              <img src="http://image.tmdb.org/t/p/w185/${movieImage}" alt="${movieTitle}" />
+              <p><strong>Summary:</strong> ${movieSummary}</p>
+              <p><strong>Release Date:</strong> ${movieRelease}</p>
+              `);
 
-              // http://www.omdbapi.com/ will give you rotten tomato rating and imdb rating if it's available
-
-              $secondHappyContainer.append(`
-                <div class="btn btn-sm btn-success" data-toggle="modal" onclick="displaySimilarMovie('${movieID}');"  data-target="${similarMovieUrl}">Similar Movies</div><br>
-
-                `);
-              }
-
-            });
-
-          }
-
-
-          $(document).ready(function() {});
-
-          function emoji(feeling) {
-            if (feeling == 1){
-              $('#genre-question .col-md-12').html("You're happy. What do you feel like watching?");
-              if (feeling == 2){
-                $('#genre-question .col-md-12').html("You're sad. What do you feel like watching?");}
-                if (feeling == 3){
-                  $('#genre-question .col-md-12').html("You're angry. What do you feel like watching?");}
-                  if (feeling == 4){
-                    $('#genre-question .col-md-12').html("You're infatuated. What do you feel like watching?");
-                  }
-                }
-              }
+            }
+          })
+        }
